891b1255e62cb782594242947c649f13c6063304
Amy Gillies <2254790g@student.gla.ac.uk>
2018-03-09 09:35:51 +0000

    fixing the folders

---
+++ computingcomms/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import url
+from computingcomms import views
+
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    url(r'^about_us/', views.about, name='about_us'),
+    url(r'^contact_us/', views.contact, name='contact_us'),
+    url(r'^quizzes/', views.quizzes, name='quizzes'),
+    url(r'^jp2/', views.jp2, name='jp2'),
+    url(r'^oose2/', views.oose2, name='oose2'),
+    url(r'^wad2/', views.wad2, name='wad2'),
+    url(r'^ads2/', views.ads2, name='ads2'),
+    url(r'^cs2t/', views.cs2t, name='cs2t'),
+    url(r'^af2/', views.af2, name='af2'),
+    url(r'^login/', views.user_login, name='login'),
+    url(r'^myaccount/', views.my_account, name='myaccount'),
+    url(r'^myquestions/', views.my_questions, name='myquestions'),
+    url(r'^mycomments/', views.my_comments, name='mycomments'),
+    url(r'^faq.', views.faq, name='faq'),
+    url(r'^forum', views.forum, name='forum'),
+    url(r'^register', views.register, name='register'),
+]
---
+++ computingcomms/views.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, 'computingcomms/home.html', {})
+
+def about(request):
+    return render(request, 'computingcomms/about_us.html', {})
+
+def quizzes(request):
+    return render(request, 'computingcomms/quizzes.html', {})
+
+def jp2(request):
+    return HttpResponse("Computing Comms - Java Programming 2 Quiz")
+
+def oose2(request):
+    return HttpResponse("Computing Comms - Object Orientated Software Engineering Quiz")
+
+def wad2(request):
+    return HttpResponse("Computing Comms - Web App Development 2 Quiz")
+
+def ads2(request):
+    return HttpResponse("Computing Comms - Algorithms and Data Structures 2 Quiz")
+
+def cs2t(request):
+    return HttpResponse("Computing Comms - Computing Systems 2 Quiz")
+
+def af2(request):
+    return HttpResponse("Computing Comms - Algorithmic Foundations 2 Quiz")
+
+def forum(request):
+    return HttpResponse("Computing Comms - Forum")
+
+def about(request):
+    return HttpResponse("Computing Comms - About Us")
+
+def contact(request):
+    return HttpResponse("Computing Comms - Contact Us")
+
+def faq(request):
+    return HttpResponse("Computing Comms - Frequently Asked Questions")
+
+def user_login(request):
+    return HttpResponse("Computing Comms - Login")
+
+def my_account(request):
+    return HttpResponse("Computing Comms - My Account")
+
+def register(request):
+    return HttpResponse("Computing Comms - Register")
+
+def my_questions(request):
+    return HttpResponse("Computing Comms - My Questions")
+
+def my_comments(request):
+    return HttpResponse("Computing Comms - My Comments")
